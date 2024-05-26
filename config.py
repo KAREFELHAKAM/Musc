eba0236e432682f8796fae711f041a5e21939cf3
KAREFELHAKAM <medoalmajak@gmail.com>
2024-05-26 16:53:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nornorno/muezk-nour",
+    "https://github.com/KAREFELHAKAM/Musc",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
